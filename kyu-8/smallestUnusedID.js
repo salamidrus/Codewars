3791755f7defe982759bd0da23a4c7aa3f1f0374
salamidrus <idrussalam95gmail.com>
2021-02-10 16:06:28 +0700

    Add Alternative Solution

--- kyu-8/smallestUnusedID.js
+++ kyu-8/smallestUnusedID.js
@@ -3,3 +3,13 @@ function nextId(ids) {
   while (ids.includes(x)) x++;
   return x;
 }
+
+// alternative solution
+function nextId(ids) {
+  for (let i = 0; i < ids.length; i++) {
+    if (ids.indexOf(i) == -1) {
+      return i;
+    }
+  }
+  return ids.length;
+}
