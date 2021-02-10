5ef4b93135e876eeddd2f984ebe99602f1c27102
salamidrus <idrussalam95gmail.com>
2021-02-10 17:07:40 +0700

    Find the position

---
+++ kyu-8/findThePosition.js
@@ -0,0 +1,39 @@
+function position(letter) {
+  //Write your own Code!
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  return "Position of alphabet: " + (alphabet.indexOf(letter) + 1);
+}
+
+// improved solution
+function position(letter) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return "Position of alphabet: " + (alphabet.indexOf(letter) + 1);
+}
