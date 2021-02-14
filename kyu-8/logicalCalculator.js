34fe5164c0022dedcd350f61ddd632e51c40ebe0
salamidrus <idrussalam95gmail.com>
2021-02-14 13:53:45 +0700

    Logical Calculator Improved Solution

--- kyu-8/logicalCalculator.js
+++ kyu-8/logicalCalculator.js
@@ -4,3 +4,14 @@ function logicalCalc(array, op) {
   if (op == "OR") return array.reduce((item, current) => item || current);
   if (op == "XOR") return array.reduce((item, current) => item != current);
 }
+
+// improved solution
+var ops = {
+  AND: (a, b) => a && b,
+  OR: (a, b) => a || b,
+  XOR: (a, b) => a !== b,
+};
+
+function logicalCalc(array, op) {
+  return array.reduce(ops[op]);
+}
